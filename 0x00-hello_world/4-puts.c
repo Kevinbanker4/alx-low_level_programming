94834be2f370cb798962b69314ae6d43ed714045
kevinbanker4 <kevinbanker4@gmail.comls>
2023-03-16 19:50:32 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,1 @@
-#include <stdio.h>
 
-/**
- * main - this is the main function 
- *
- * Return: always 0
- */
-
-int main(void)
-{	
-	puts("\n"Programming is like building a multilingual puzzle");
-	return (0);
-}
