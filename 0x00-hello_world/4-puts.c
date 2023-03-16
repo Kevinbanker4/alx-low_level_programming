089c00fc43ea4c6d07223de1eb0c213384823a6c
kevinbanker4 <kevinbanker4@gmail.comls>
2023-03-16 18:19:53 +0300

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ /**
+  * main-this is the main fuction 
+  * 
+  * Return-means success and returning a non-zero number
+ */
+int main(void)
+{
+             puts ("\"Programming is like building a multilingual puzzle");
+             return (0);
+}
